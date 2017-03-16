f1dd443828bbaa3c7560f7f979efb772bfaef407
Trey Tacon <ttacon@gmail.com>
2017-03-16 11:27:39 -0400

    Initial prototype version

---
+++ index.js
@@ -0,0 +1,72 @@
+const _ = require('underscore');
+const AWSXRay = require('aws-xray-sdk');
+
+module.exports.captureMongo = function captureMongo(db) {
+  // We've already wrapped it.
+  if (db.__proto__.__collection) return db;
+
+  patchCollection(db);
+  return db;
+};
+
+function patchCollection(db) {
+  // Because Mongojs adds that annoying proxy for all `get`s.
+  db.__proto__.__collection = db.__proto__.collection;
+  db.__proto__.collection = function patchedCollection() {
+    const collection = db.__collection.apply(db, arguments);
+
+    patchQueries(collection);
+
+    return collection;
+  };
+}
+
+const functions = [
+  'find',
+  'findOne',
+  'update',
+  'remove',
+  'aggregate'
+];
+
+function patchQueries(coll) {
+  functions.forEach(function(fn) {
+    const baseName = `__${fn}`;
+    coll[baseName] = coll[fn];
+    coll[fn] = function() {
+      var parent = AWSXRay.getSegment();
+
+      if (!parent) {
+        return coll[baseName].apply(this, arguments);
+      }
+
+      var subsegment = parent.addNewSubsegment(coll._name);
+
+      let hasCb = false;
+      var args = Array.from(arguments);
+      if (args.length && _.isFunction(args[args.length-1])) {
+        hasCb = true;
+        const cb = args[args.length-1];
+        args[args.length-1] = function(err, data) {
+          cb(err, data);
+          subsegment.close(err);
+        };
+      }
+
+      var query = coll[baseName].apply(this, args);
+      if (!hasCb) {
+        var oldNext = coll[baseName].next;
+        coll[baseName].next = function next(fn) {
+          subsegment.close();
+          oldNext.apply(coll[baseName], fn);
+        };
+      }
+
+      subsegment.addMetadata('collection', coll._name);
+      subsegment.addMetadata('query', fn);
+      // TODO(ttacon): Add query fingerprint.
+
+      return query;
+    };
+  });
+}
